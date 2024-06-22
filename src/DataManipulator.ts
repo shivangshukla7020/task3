f4d597d8978f4586453ed14237ce135f2dfe6b59
shivangshukla7020 <shivangshukla201@gmail.com>
2024-06-23 02:01:56 +0530

    Helping tools were added successfully

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,35 @@
 import { ServerRespond } from './DataStreamer';
 
-export interface Row {
-  stock: string,
-  top_ask_price: number,
+export interface Row { //As now we have new schema we need to implement similar structure here as same kind of structure must be returned by generateRow function
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+  // stock: string,
+  // top_ask_price: number,
+  // timestamp: Date,
 }
 
 
-export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+export class DataManipulator { // Here we made the changes with required logic for calculation of ration, 10% annual upper and lower bound, alert and returned it as ROW structure
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = priceABC/priceDEF;
+    const upperBound = 1 + 0.10;
+    const lowerBound = 1 - 0.10;
+    return{
+      price_abc:priceABC,
+      price_def:priceDEF,
+      ratio,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?serverResponds[0].timestamp : serverResponds[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio>upperBound || ratio < lowerBound) ? ratio: undefined,
+    };
+   
   }
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -18,15 +18,23 @@ class Graph extends Component<IProps, {}> {
     return React.createElement('perspective-viewer');
   }
 
+  //The graph with ratio and bounds will visually help the trader to predict the stock span and the alert will help making the quick decision
   componentDidMount() {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
-    const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+    const schema = {  //The task is to track their ratio along with upper bound and lower bound we need to make changes in schema
+      price_abc: 'float',  //Price of stock abc to calculate ratio
+      price_def: 'float',  //Price of stock def to calculate ratio
+      ratio: 'float',    
       timestamp: 'date',
+      upper_bound: 'float',  //To set upper bound for tracking the ratio
+      lower_bound: 'float',  //To set lower bound for tracking the ratio
+      trigger_alert: 'float', //An alert that is generated when bounds are crossed
+      // stock: 'string',
+      // top_ask_price: 'float',
+      // top_bid_price: 'float',
+      // timestamp: 'date',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,23 +44,26 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      //elem.setAttribute('column-pivots', '["stock"]');  //Not required as we need ratio only not the prices
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio","lower_bound","upper_bound","trigger_alert"]'); //Ratio, lower bound, upper bound and alert are added in column attribute
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ]as unknown as TableData);
     }
   }
 }
